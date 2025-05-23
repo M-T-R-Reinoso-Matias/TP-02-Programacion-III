38297b03cecd1444b788d15198ead002b490e1c1
Reinoso Angel Matias <matias_reinoso@outlook.com>
2025-05-22 21:34:45 -0300

    Modificaciones Esteticas

--- ejercicio4/script.js
+++ ejercicio4/script.js
@@ -1,10 +1,17 @@
 document.getElementById("calcular").onclick = () => {
   let peso = parseFloat(document.getElementById("peso").value);
   let est = parseFloat(document.getElementById("estatura").value);
-  if (!peso || !est) {
-    alert("Faltan datos válidos");
+
+  if (isNaN(peso) || peso <= 0) {
+    alert("Ingrese un peso válido en kilogramos.");
     return;
   }
+
+  if (isNaN(est) || est <= 0 || est > 2.5) {
+    alert("Ingrese una estatura válida en metros (por ejemplo: 1.75). No use centímetros.");
+    return;
+  }
+
   const imc = peso / (est * est);
   alert("IMC: " + imc.toFixed(2));
 };
